64567f340efa5bf2199854e05a4755215e634dae
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-02-01 00:16:57 -0700

    Doing it better!

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-import React, { useState } from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-// import Gallery from "./componets/Gallery";
-import ContactForm from "./components/Contact";
-import Footer from "./components/Footer"
-import Portfolio from "./components/Portfolio";
+import React from "react";
+import { StoreProvider } from "./utils/GlobalState";
 
 function App() {
-  const [categories] = useState([
-    {
-      name: "Portfolio",
-      description: "All my work",
-    },
-    { name: "Photos", description: "Some Pictures I have taken" },
-    { name: "Resume", description: "What I Have Done" },
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
-
-  // function Foot() {}
-
-
-
   return (
-    <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-            {/* <Gallery currentCategory={currentCategory}></Gallery> */}
-            <About></About>
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        ) : }
-        
-        
-      </main>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div>
+        <StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+          </Switch>
+        </StoreProvider>
+      </div>
+    </Router>
   );
 }
 
